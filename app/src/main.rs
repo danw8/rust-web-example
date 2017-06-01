64bebfc6f54d759477ceb0b20d88e8487c74456c
Daniel Wilkins <daniel.wilkins@centeva.com>
2017-06-01 15:14:32 -0600

    http request on client side created

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,10 +2,13 @@
 #![plugin(maud_macros)]
 
 extern crate maud;
-extern crate webplatform;
+extern crate libc;
+#[macro_use] extern crate webplatform;
 
 //use std::sync::{Arc, Mutex};
 use std::rc::Rc;
+use std::str;
+use std::ffi::{CString, CStr};
 use maud::{Markup, PreEscaped};
 use webplatform::{Event, Document, HtmlNode};
 
@@ -17,6 +20,19 @@ macro_rules! enclose {
             $y
         }
     };
+}
+
+trait Interop {
+    fn as_int(self, _:&mut Vec<CString>) -> libc::c_int;
+}
+
+impl<'a> Interop for &'a str {
+    fn as_int(self, arena:&mut Vec<CString>) -> libc::c_int {
+        let c = CString::new(self).unwrap();
+        let ret = c.as_ptr() as libc::c_int;
+        arena.push(c);
+        return ret;
+    }
 }
 
 fn main(){
@@ -57,9 +73,11 @@ fn route(mut location: String, app: HtmlNode){
 }
 
 fn member_home() -> Markup{
+    let username = http_get("getuser");
+    println!("{}", username);
     html! {
         h1 {
-            "Welcome, Member!"
+            "Welcome, " (username)
         }
     }
 }
@@ -74,5 +92,18 @@ fn member_list() -> Markup{
             li { "item 2"}
             li { "item 3"}
         }
+    }
+}
+
+fn http_get(url: &str) -> String{
+    let a = js!{ (url) b"\
+            var xmlHttp = new XMLHttpRequest();\
+            xmlHttp.open( 'GET', UTF8ToString($0), false );\
+            xmlHttp.send( null );\
+            return allocate(intArrayFromString(xmlHttp.responseText), 'i8', ALLOC_STACK);\
+        \0" };
+    println!("{:?}", a);
+    unsafe {
+        str::from_utf8(CStr::from_ptr(a as *const libc::c_char).to_bytes()).unwrap().to_owned()
     }
 }
--- src/route/member.rs
+++ src/route/member.rs
@@ -12,6 +12,14 @@ fn member(user_service: UserService) -> Result<Markup, Flash<Redirect>> {
 	Ok(view::member::member(user_service.user))
 }
 
+#[get("/getuser")]
+fn getuser(user_service: UserService) -> String {
+	if let Some(u) = user_service.user {
+		return u.username;
+	}
+	String::new()
+}
+
 // #[get("/member/<path..>")]
 // fn member_path(user_service: UserService, path: PathBuf) -> Result<Markup, Flash<Redirect>> {
 // 	if user_service.user.is_none() {
